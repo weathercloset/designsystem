843e9ef4252d1b33d23c3b640cbbee08a7f8b25a
Jonghyeon Ko <flowithusdev@gmail.com>
2022-01-19 01:29:04 +0900

    🔧 디자인시스템 초기 세팅

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as Button } from './components/Button'
+export { default as Header } from './components/Header'
+export { default as Page } from './components/Page'
---
+++ typings/declation.d.ts
@@ -0,0 +1,5 @@
+// typings/declaration.d.ts
+declare module '*.scss' {
+  const content: { [className: string]: string }
+  export = content
+}
