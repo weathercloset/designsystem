6da80a2a92a449432e2c1d5dff3e6f2536929e91
마누 <manu.k@kakaocorp.com>
2022-06-08 09:49:25 +0900

    fix(react, react-dom): version 높이기

--- index.ts
+++ index.ts
@@ -1,3 +1,1 @@
-export { default as Button } from './components/Button'
-export { default as Header } from './components/Header'
-export { default as Page } from './components/Page'
+export { Button, Header, Page } from './components'
